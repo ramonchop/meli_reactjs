f1ed6803351c9e349b8ac00438eab9a881581045
Ramon Eduardo Estay <restay@gmail.com>
2023-06-08 03:21:20 -0400

    add node server url

--- src/presentation/product_detail/ProductDetailPage.js
+++ src/presentation/product_detail/ProductDetailPage.js
@@ -7,99 +7,14 @@ export async function loader({ params }) {
 
     const productId = params.id
 
-    const productUrl = `https://api.mercadolibre.com/items/${productId}`;
-    const descriptionUrl = `${productUrl}/description`;
-
-    const itemsResponse = await axios.get(productUrl);
-
+    const itemsResponse = await axios.get(`http://localhost:8181/api/items/${productId}`);
     if (itemsResponse.status !== 200) {
         throw new Error("Error");
     }
-
-    const descriptionResponse = await axios.get(descriptionUrl);
-
-    if (descriptionResponse.status !== 200) {
-        throw new Error("Error");
-    }
-
-
-    const itemsData = itemsResponse.data
-    const descriptionData = descriptionResponse.data
-
-    const description = descriptionData.plain_text;
-
-    const currencyId = itemsData.currency_id;
-    const address = itemsData.seller_address;
-    const countryId = address.country.id;
-    const categoryId = itemsData.category_id;
-
-    const currencyResponse = await axios.get(`https://api.mercadolibre.com/currencies/${currencyId}`);
-
-    if (currencyResponse.status !== 200) {
-        throw new Error("Error");
-    }
-
-    const localeResponse = await axios.get(`https://api.mercadolibre.com/countries/${countryId}`);
-
-    if (localeResponse.status !== 200) {
-        throw new Error("Error");
-    }
-
-
-    const categoryResponse = await axios.get(`https://api.mercadolibre.com/categories/${categoryId}`);
-
-    if (categoryResponse.status !== 200) {
-        throw new Error("Error");
-    }
-
-
-    let picture = itemsData.secure_thumbnail;
-
-    if (itemsData.pictures !== undefined && itemsData.pictures.length > 0){
-        picture = itemsData.pictures[0].secure_url ?? picture;
-    }
-    const currencyData = currencyResponse.data
-    const localeData = localeResponse.data
-    const locale = localeData.locale.replace("_", "-");
-    const categoryData = categoryResponse.data
-
-    const price = {
-        "currency": currencyId,
-        "amount": itemsData.price,
-        "decimals": currencyData.decimal_places,
-    }
-    
-    const pathFromRoot = categoryData.path_from_root;
-
-    
-
-    const id = itemsData.id
-    const title = itemsData.title
-    const soldQuantity = itemsData.sold_quantity
-    const condition = itemsData.condition
-    const freeShipping = itemsData.shipping.free_shipping
-
-    const result = {
-        author: {
-            name: "Ramon",
-            lastname: "Estay",
-        },
-        item: {
-            id: id,
-            title: title,
-            price: price,
-            picture: picture,
-            condition: condition,
-            free_shipping: freeShipping,
-            sold_quantity: soldQuantity,
-            locale: locale,
-            description: description
-        },
-        path_from_root: pathFromRoot,
-    }
-
+    const result = itemsResponse.data;
 
     return result;
+
 
 }
 
--- src/presentation/search_results/SearchResultsPage.js
+++ src/presentation/search_results/SearchResultsPage.js
@@ -8,79 +8,11 @@ export async function loader({ request }) {
     const url = new URL(request.url);
 
     const search = url.searchParams.get("search");
-    const itemsResponse = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}&limit=4`);
-
+    const itemsResponse = await axios.get(`http://localhost:8181/api/items?q=${search}`);
     if (itemsResponse.status !== 200) {
         throw new Error("Error");
     }
-    const itemsData = itemsResponse.data
-    let categoriesList = [];
-    let pathFromRoot = [];
-    if (itemsData.filters !== undefined && itemsData.filters.length > 0){
-        categoriesList = itemsData.filters.find((filter) => filter.id === "category").values;
-        pathFromRoot = categoriesList[0].path_from_root;
-    } else if (itemsData.available_filters !== undefined && itemsData.available_filters.length > 0) {
-        categoriesList = itemsData.available_filters.find((filter) => filter.id === "category").values;
-        const currentCategory = categoriesList.reduce((prev, current) => (prev.results > current.results) ? prev : current).id;
-        const categoryPathResponse = await axios.get(`https://api.mercadolibre.com/categories/${currentCategory}`);
-        if (categoryPathResponse.status !== 200) {
-            throw new Error("Error");
-        }
-        const categoryPathData = categoryPathResponse.data
-        pathFromRoot = categoryPathData.path_from_root;
-    
-    }
-    const categories = categoriesList.map((category) => { return category.id });
-
-    const result = {
-        "author": {
-            "name": "Ramon",
-            "lastname": "Estay",
-        },
-        path_from_root: pathFromRoot,
-        categories: categories.map((category) => { return category.name }), 
-        items: await Promise.all(itemsData.results.map(async (item) => {
-            const currencyId = item.currency_id;
-            const address = item.seller_address;
-            const countryId = address.country.id;
-
-            const currencyResponse = await axios.get(`https://api.mercadolibre.com/currencies/${currencyId}`);
-
-            if (currencyResponse.status !== 200) {
-                throw new Error("Error");
-            }
-
-            const localeResponse = await axios.get(`https://api.mercadolibre.com/countries/${countryId}`);
-
-            if (localeResponse.status !== 200) {
-                throw new Error("Error");
-            }
-
-            const currencyData = currencyResponse.data
-            const localeData = localeResponse.data
-            const locale = localeData.locale.replace("_", "-");
-
-            const price = {
-                "currency": currencyId,
-                "amount": item.price,
-                "decimals": currencyData.decimal_places,
-            }
-
-            const state = address.state.name;
-
-            return {
-                id: item.id,
-                title: item.title,
-                price: price,
-                picture: item.thumbnail,
-                condition: item.condition,
-                free_shipping: item.shipping.free_shipping,
-                locale: locale,
-                state: state,
-            }
-        })),
-    }
-
+    const result = itemsResponse.data;
 
     return result;
 
